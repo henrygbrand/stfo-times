44f5b70010779e5dc346e2b06e09570dbcfe1d04
Henry <hbrand@vols.utk.edu>
2023-10-31 16:27:58 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const MONGODB_URI = 'mongodb://localhost:27017';
+const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
