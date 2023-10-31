d36725e7032fa84ca084c71dea90fd3f5ac8d941
Henry <hbrand@vols.utk.edu>
2023-10-31 14:51:35 -0400

    initial

---
+++ public/script.js
@@ -0,0 +1,105 @@
+const tableBody = document.getElementById('tableBody');
+const addForm = document.getElementById('addForm');
+const deleteForm = document.getElementById('deleteForm');
+
+const fetchData = async () => {
+  const response = await fetch('/data');
+  const data = await response.json();
+  tableBody.innerHTML = data.map(item => `<tr><td>${item.username}</td><td>${item.time}</td></tr>`).join('');
+};
+
+addForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(addForm);
+  await fetch('/addOrUpdate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: formData.get('username'),
+      time: Number(formData.get('time'))
+    })
+  });
+  fetchData();
+  addForm.reset();
+});
+
+deleteForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(deleteForm);
+  await fetch('/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: formData.get('username')
+    })
+  });
+  fetchData();
+  deleteForm.reset();
+});
+
+// Function to fetch UserId and update the table
+async function updateUserRow(row) {
+    const usernameCell = row.querySelector("td:first-child");
+    const userIdCell = document.createElement('td');
+
+    const username = usernameCell.textContent;
+
+    const response = await fetch('https://users.roproxy.com/v1/usernames/users', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            usernames: [username],
+            excludeBannedUsers: true
+        })
+    });
+
+    const data = await response.json();
+
+    if (data.data && data.data.length > 0) {
+        const thumbnailResponse = await fetch(`https://thumbnails.roproxy.com/v1/users/avatar?userIds=${data.data[0].id}&size=140x140&format=Png&isCircular=false`);
+        const thumbnailData = await thumbnailResponse.json();
+    
+        if (thumbnailData.data && thumbnailData.data.length > 0) {
+            const imageUrl = thumbnailData.data[0].imageUrl;
+            
+            // Create image element and set its properties
+            const userImage = document.createElement('img');
+            userImage.src = imageUrl;
+            userImage.alt = "User's avatar";
+            userImage.width = 140;
+            userImage.height = 140;
+            
+            // Append image to the third column
+            userIdCell.innerHTML = ''; // Clear the content (if there's any)
+            userIdCell.appendChild(userImage);
+        }
+    
+        row.appendChild(userIdCell);
+    }
+}
+
+
+// Start observing the table
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        if (mutation.type === "childList") {
+            mutation.addedNodes.forEach(node => {
+                if (node.nodeType === Node.ELEMENT_NODE && node.tagName === "TR") {
+                    updateUserRow(node);
+                }
+            });
+        }
+    });
+});
+
+observer.observe(tableBody, { childList: true });
+
+
+fetchData();
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const MONGODB_URI = 'mongodb://localhost:27017';
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB!');
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    time: Number
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+let data = [];
+
+app.get('/data', async (req, res) => {
+    const users = await User.find().sort({ time: -1 });  // Sorting by time, descending.
+    res.json(users);
+  });
+  
+app.post('/addOrUpdate', async (req, res) => {
+    const { username, time } = req.body;
+  
+    const user = await User.findOne({ username });
+    if (user) {
+      user.time = time;
+      await user.save();
+    } else {
+      const newUser = new User({ username, time });
+      await newUser.save();
+    }
+  
+    res.json({ success: true });
+  });
+  
+app.post('/delete', async (req, res) => {
+    const { username } = req.body;
+    await User.deleteOne({ username });
+    res.json({ success: true });
+  });
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
